4a6d6974e88a6cb7478efe7f81e36cb344925f67
Captain Springer <1554849150@qq.com>
2020-07-26 19:34:12 +0800

    改进源码，增加注释
    改进了源码，增加了注释

--- ChangeHosts.py
+++ ChangeHosts.py
@@ -26,13 +26,15 @@ hosts=[]
 with open(hostsfile,'r') as fd:
     hosts=fd.readlines()
 
-#修改hosts中第23行到41行（具体要修改多长，需要自己在hosts文件里面看长度，这里我的是23到41）
+#删除hosts中第23行到41行（具体要修改多长，需要自己在hosts文件里面看长度，这里我的是23到40行）
 for i in range(23,41)[::-1]:
     del hosts[i]
 
+#在被删除原github hosts的区域增加新的github hosts
 hosts.append(nh)
+#把列表内容全部连接起来
 hosts=''.join(hosts)
-
+#复写入文件
 with open('C:/Windows/System32/drivers/etc/hosts','w') as fd:
     fd.write(hosts)
 
