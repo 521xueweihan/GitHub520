a0d3b39bd940f721bf188b9f540d105e1c144fee
Lee SunBowen <1554849150@qq.com>
2020-07-26 19:04:43 +0800

    修改了源码，之前的版本虽然功能依然能实现，但显示有误

--- ChangeHosts.py
+++ ChangeHosts.py
@@ -1,6 +1,3 @@
-import re
-import os
-import sys
 import requests
 import bs4
 import time
@@ -14,9 +11,9 @@ r = requests.get(url,timeout=12)
 r.encoding = 'utf-8'
 t = r.text
 soup = bs4.BeautifulSoup(t,'html.parser')
-nh = []
 #爬虫结束
 #取得hosts
+nh = []
 for i in soup.find_all('pre'):
     nh.append(i.text)
 #nh转换为列表形式
@@ -26,15 +23,14 @@ hosts=[]
 with open(hostsfile,'r') as fd:
     hosts=fd.readlines()
 
-#修改hosts中第23行到41行（具体要修改哪些行数，需要自己在hosts文件里面看，这里我的是23到41行）
-for i in range(23,41):
-    hosts[i] = nh[i-23]
-
+#修改hosts中第23行到41行（具体要修改多长，需要自己在hosts文件里面看长度，这里我的是23到41）
+for i in range(23,41)[::-1]:
+    del hosts[i]
 
 hosts.append(nh)
 hosts=''.join(hosts)
 
-with open('C:/Windows/System32/drivers/etc/hosts','w') as fd:
+with open('C:/Windows/System32/drivers/etc/hosts - 副本','w') as fd:
     fd.write(hosts)
 
 print('修改成功,3秒后自动关闭')
