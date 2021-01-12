fd1885c513adaa4b7dba169bc3bec67fe0f4d8fc
xueweihan <595666367@qq.com>
2021-01-12 23:20:29 +0800

    update ip list

--- fetch_ips.py
+++ fetch_ips.py
@@ -41,7 +41,6 @@ RAW_URL = [
     "github-production-release-asset-2e65be.s3.amazonaws.com",
     "github-production-user-asset-6210df.s3.amazonaws.com",
     "github-production-repository-file-5c1aeb.s3.amazonaws.com",
-    "www.githubstatus.com",
     "githubstatus.com",
     "github.community"]
 
