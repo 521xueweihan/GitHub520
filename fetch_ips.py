f9cfa4b9e356e46069cedf7a7b55f0242f9df621
xueweihan <595666367@qq.com>
2022-11-22 14:13:08 +0800

    update

--- fetch_ips.py
+++ fetch_ips.py
@@ -16,30 +16,30 @@ from pythonping import ping
 from requests_html import HTMLSession
 from retry import retry
 
-GITHUB_URLS = [
-    'alive.github.com'
-]
-
 # GITHUB_URLS = [
-#     'alive.github.com', 'api.github.com', 'assets-cdn.github.com',
-#     'avatars.githubusercontent.com', 'avatars0.githubusercontent.com',
-#     'avatars1.githubusercontent.com', 'avatars2.githubusercontent.com',
-#     'avatars3.githubusercontent.com', 'avatars4.githubusercontent.com',
-#     'avatars5.githubusercontent.com', 'camo.githubusercontent.com',
-#     'central.github.com', 'cloud.githubusercontent.com', 'codeload.github.com',
-#     'collector.github.com', 'desktop.githubusercontent.com',
-#     'favicons.githubusercontent.com', 'gist.github.com',
-#     'github-cloud.s3.amazonaws.com', 'github-com.s3.amazonaws.com',
-#     'github-production-release-asset-2e65be.s3.amazonaws.com',
-#     'github-production-repository-file-5c1aeb.s3.amazonaws.com',
-#     'github-production-user-asset-6210df.s3.amazonaws.com', 'github.blog',
-#     'github.com', 'github.community', 'github.githubassets.com',
-#     'github.global.ssl.fastly.net', 'github.io', 'github.map.fastly.net',
-#     'githubstatus.com', 'live.github.com', 'media.githubusercontent.com',
-#     'objects.githubusercontent.com', 'pipelines.actions.githubusercontent.com',
-#     'raw.githubusercontent.com', 'user-images.githubusercontent.com',
-#     'vscode.dev'
+#     'alive.github.com'
 # ]
+
+GITHUB_URLS = [
+    'alive.github.com', 'api.github.com', 'assets-cdn.github.com',
+    'avatars.githubusercontent.com', 'avatars0.githubusercontent.com',
+    'avatars1.githubusercontent.com', 'avatars2.githubusercontent.com',
+    'avatars3.githubusercontent.com', 'avatars4.githubusercontent.com',
+    'avatars5.githubusercontent.com', 'camo.githubusercontent.com',
+    'central.github.com', 'cloud.githubusercontent.com', 'codeload.github.com',
+    'collector.github.com', 'desktop.githubusercontent.com',
+    'favicons.githubusercontent.com', 'gist.github.com',
+    'github-cloud.s3.amazonaws.com', 'github-com.s3.amazonaws.com',
+    'github-production-release-asset-2e65be.s3.amazonaws.com',
+    'github-production-repository-file-5c1aeb.s3.amazonaws.com',
+    'github-production-user-asset-6210df.s3.amazonaws.com', 'github.blog',
+    'github.com', 'github.community', 'github.githubassets.com',
+    'github.global.ssl.fastly.net', 'github.io', 'github.map.fastly.net',
+    'githubstatus.com', 'live.github.com', 'media.githubusercontent.com',
+    'objects.githubusercontent.com', 'pipelines.actions.githubusercontent.com',
+    'raw.githubusercontent.com', 'user-images.githubusercontent.com',
+    'vscode.dev'
+]
 
 HOSTS_TEMPLATE = """# GitHub520 Host Start
 {content}
@@ -66,8 +66,6 @@ def write_file(hosts_content: str, update_time: str) -> bool:
                 if old_hosts == hosts_content_hosts:
                     print("host not change")
                     return False
-            else:
-                return False
 
     with open(template_path, "r") as temp_fb:
         template_str = temp_fb.read()
